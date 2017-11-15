4eb7c0ae2157654ee49ac589eda121f5ce0c90b8
Erika Estrada <eestradamedina@go.pasadena.edu>
2017-11-15 14:35:09 -0800

    Created Global class

---
+++ src/models/Global.java
@@ -0,0 +1,26 @@
+package models;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import structure.School;
+
+public class Global {
+
+	ArrayList<User> users;
+	ArrayList<School> database;
+	
+	/*
+	 * Constructors
+	 */
+	
+	public Global() {}
+	
+	public Global(ArrayList<User> users) {
+		
+	}
+	
+	public Global(ArrayList<User> users, File input) {
+		
+	}
+}
